c2662db690df2ce2035c923cb41229ca7a5325b1
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-12 19:14:38 -0600

    Implementing Repository

--- OMS.DataAccess/Data/Repository/Repository.cs
+++ OMS.DataAccess/Data/Repository/Repository.cs
@@ -1,12 +1,82 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using OMS.DataAccess.Data.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OMS.DataAccess.Data.Repository
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly DbContext _Context;
+        internal DbSet<T> dbSet;
+
+        public Repository(DbContext context)
+        {
+            _Context = context;
+            this.dbSet = context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluideProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (incluideProperties!=null)
+            {
+                foreach (var includeProperty in incluideProperties.Split(new char[] { ','},StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            return query.ToList();
+
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string incluideProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (incluideProperties != null)
+            {
+                foreach (var includeProperty in incluideProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            T entityToRemove = dbSet.Find(id);
+            Remove(entityToRemove);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
     }
 }
