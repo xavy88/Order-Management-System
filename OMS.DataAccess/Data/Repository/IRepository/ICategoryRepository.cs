86eb14ef2040732a58a2e225fdd85547797e29b0
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-12 19:19:49 -0600

    Implementing ICategory Repository

--- OMS.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
+++ OMS.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using OMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace OMS.DataAccess.Data.Repository.IRepository
 {
-    public interface ICategoryRepository
+    public interface ICategoryRepository:IRepository<Category>
     {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+        void Update(Category category);
+
     }
 }
