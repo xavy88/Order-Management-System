f5c36b3e6d62ed405c769405398efae7d14da3f0
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-12 19:32:16 -0600

    Implementing Category Repository

--- OMS.DataAccess/Data/ApplicationDbContext.cs
+++ OMS.DataAccess/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OMS.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,7 @@ namespace OMS.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Category { get; set; };
     }
 }
--- OMS.DataAccess/Data/Repository/CategoryRepository.cs
+++ OMS.DataAccess/Data/Repository/CategoryRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using OMS.DataAccess.Data.Repository.IRepository;
+using OMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace OMS.DataAccess.Data.Repository
 {
-    public class CategoryRepository
+    public class CategoryRepository:Repository<Category>,ICategoryRepository
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetCategoryListForDropDown()
+        {
+            return _db.Category.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+        public void Update(Category category)
+        {
+                var objFromDb = _db.Category.FirstOrDefault(c => c.Id == category.Id);
+                objFromDb.Name = category.Name;
+                objFromDb.Status = category.Status;
+
+                _db.SaveChanges();
+
+        }
     }
 }
