02a52a474b50463978794f31db0843b0d0145e52
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-16 16:00:04 -0600

    Upsert Post Action Method

--- OMS/Areas/Admin/Controllers/CategoryController.cs
+++ OMS/Areas/Admin/Controllers/CategoryController.cs
@@ -39,6 +39,27 @@ namespace OMS.Areas.Admin.Controllers
             return View(category);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(category);
+        }
+
         #region API CALLS
         [HttpGet]
 
