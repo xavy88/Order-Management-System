8aefa15f5f8427086c8e814fe7e7adeb24f5b04c
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-17 20:05:16 -0600

    Add Service Index API and Service View Mode

---
+++ OMS.Models/ViewModel/ServiceVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.Models.ViewModel
+{
+    public class ServiceVM
+    {
+        public Service Service { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
---
+++ OMS/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using OMS.DataAccess.Data.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OMS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Service.GetAll(incluideProperties:"Category") });
+        }
+
+        #endregion
+
+
+    }
+}
