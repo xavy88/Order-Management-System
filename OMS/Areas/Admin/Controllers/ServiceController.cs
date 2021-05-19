59c0959b82112859c1c411380c3fd22d275440f1
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-18 20:49:01 -0600

    Adding the API for Delete into Service Controller

--- OMS/Areas/Admin/Controllers/ServiceController.cs
+++ OMS/Areas/Admin/Controllers/ServiceController.cs
@@ -103,6 +103,7 @@ namespace OMS.Areas.Admin.Controllers
             }
             else
             {
+                SerVM.CategoryList = _unitOfWork.Category.GetCategoryListForDropDown();
                 return View(SerVM);
             }
         }
@@ -111,6 +112,27 @@ namespace OMS.Areas.Admin.Controllers
         public IActionResult GetAll()
         {
             return Json(new { data = _unitOfWork.Service.GetAll(incluideProperties:"Category") });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var serviceFromDb = _unitOfWork.Service.Get(id);
+            string webRootPath = _hostEnvironment.WebRootPath;
+            var imagePath = Path.Combine(webRootPath, serviceFromDb.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(imagePath))
+            {
+                System.IO.File.Delete(imagePath);
+            }
+
+            if (serviceFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+
+            _unitOfWork.Service.Remove(serviceFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Deleted Successfully." });
         }
 
         #endregion
