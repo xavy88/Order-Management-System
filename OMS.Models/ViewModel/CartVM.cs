712be511fe238ed2f5265ab16099226d1839425c
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-20 20:32:23 -0600

    Adding Cart View Model

---
+++ OMS.Models/ViewModel/CartVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.Models.ViewModel
+{
+    public class CartVM
+    {
+        public IList<Service> ServicesList { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
+    }
+}
