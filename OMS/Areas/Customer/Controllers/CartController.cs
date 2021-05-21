1fa252aec2619a28a232e5ed48624a0b9cedf85c
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-20 20:45:06 -0600

    Cart Controller Index View

--- OMS/Areas/Customer/Controllers/CartController.cs
+++ OMS/Areas/Customer/Controllers/CartController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace OMS.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class CartController : Controller
     {
         public IActionResult Index()
