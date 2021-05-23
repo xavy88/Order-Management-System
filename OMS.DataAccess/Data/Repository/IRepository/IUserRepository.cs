e5be35c1514d8f7511b8dec47f31af355621ef5c
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-23 11:53:05 -0600

    Add User to repository

---
+++ OMS.DataAccess/Data/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using OMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.DataAccess.Data.Repository.IRepository
+{
+    public interface ICategoryRepository:IRepository<Category>
+    {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+        void Update(Category category);
+
+    }
+}
