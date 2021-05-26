df6eb7a384a740d20e273ed637d8402b8b5e206b
Xavy Urbina <xavyurbina88@gmail.com>
2021-05-25 19:04:54 -0600

    Adding the SP to Repository

---
+++ OMS.DataAccess/Data/Repository/IRepository/ISP_Call.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.DataAccess.Data.Repository.IRepository
+{
+    public interface ISP_Call:IDisposable
+    {
+        IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null);
+
+        void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null);
+
+        T ExecuteReturnScaler<T>(string procedureName, DynamicParameters param = null);
+    }
+}
